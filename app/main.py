9aaae740b2413c32668f6bcf08ec0b768ef2f372
Alex <midgard044@gmail.com>
2025-09-10 16:20:24 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -14,104 +14,94 @@ class Distance:
     def __add__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
-        elif isinstance(other, (int, float)):
-            self.km += other
-            return self
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        if isinstance(other, (int, float)):
+            return Distance(self.km + other)
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __iadd__(self, other: Distance | int | float) -> Distance:
         if isinstance(other, Distance):
             self.km += other.km
             return self
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             self.km += other
             return self
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __mul__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(self.km * other)
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"int, float not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"int, float not {type(other).__name__}"
+        )
 
     def __truediv__(self, other: int | float) -> Distance:
         if isinstance(other, (int, float)):
             return Distance(round(self.km / other, 2))
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"int, float not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"int, float not {type(other).__name__}"
+        )
 
     def __lt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km < other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km < other
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __gt__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km > other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km > other
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __eq__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km == other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km == other
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __le__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km <= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km <= other
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
 
     def __ge__(self, other: Distance | int | float) -> bool:
         if isinstance(other, Distance):
             return self.km >= other.km
-        elif isinstance(other, (int, float)):
+        if isinstance(other, (int, float)):
             return self.km >= other
-        else:
-            raise TypeError(
-                f"Argument 'other' must be of type"
-                f"{self.__class__.__name__}, int, float"
-                f"not {type(other).__name__}"
-            )
+        raise TypeError(
+            f"Argument 'other' must be of type"
+            f"{self.__class__.__name__}, int, float"
+            f"not {type(other).__name__}"
+        )
